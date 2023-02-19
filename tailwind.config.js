3474e603c3ba1113efd4a535731a2402218cb746
Pedro Rodrigues <58788062+PedroRBC@users.noreply.github.com>
2023-02-19 17:29:46 -0300

    add number lines to code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        app: "linear-gradient(to left, #40c9ff, #e81cff)",
-      },
       colors: {
         bg: "#040303",
       },
-      gridTemplateRows: {
-        layout: "2rem 1fr 1.5rem",
-      },
-      gridTemplateColumns: {
-        editor: "3.5rem 16rem 1fr",
-      },
+    },
+    backgroundImage: {
+      app: "linear-gradient(to left, #40c9ff, #e81cff)",
+    },
+    gridTemplateRows: {
+      layout: "2rem 1fr 1.5rem",
+    },
+    gridTemplateColumns: {
+      editor: "3.5rem 16rem 1fr",
     },
   },
   plugins: [require("tailwind-scrollbar")],
