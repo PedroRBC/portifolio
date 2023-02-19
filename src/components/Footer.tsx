944b4bd9c97ce9073f3da5628313565444228aef
Pedro Rodrigues <58788062+PedroRBC@users.noreply.github.com>
2023-02-19 12:42:24 -0300

    Update Footer Git URL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <div className="text-sm px-3 text-[#8F8CA8] flex gap-3 items-center justify-end bg-[#2a273f]">
             <a
-                href="https://github.com/PedroRBC"
+                href="https://github.com/PedroRBC/portifolio"
                 target="_blank"
                 className="flex gap-1 items-center text-xs hover:text-[#E0DEF2]"
                 rel="noreferrer"
