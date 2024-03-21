77a4e5cb9c75142b2ff16a55d06b7fa57ec2b256
PedroRBC <pedrinhorbc@gmail.com>
2024-03-21 18:22:19 +0000

    lint(fix): Pass Lint --fix

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -2,11 +2,10 @@ interface DocsConfig {
   siteName: string;
   description: string;
   siteIcon: string;
-
 }
 
 export const docsConfig: DocsConfig = {
-    siteName: "PedroRbc Portfolio",
-    description: "",
-    siteIcon: "https://avatars.githubusercontent.com/u/58788062?v=4"
+  siteName: "PedroRbc Portfolio",
+  description: "",
+  siteIcon: "https://avatars.githubusercontent.com/u/58788062?v=4",
 };
