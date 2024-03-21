d10d19f5066261ff024dc4f325330a4442449a3c
PedroRBC <pedrinhorbc@gmail.com>
2024-03-21 20:09:46 +0000

    fix: Fix Maintenance

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,12 +1,12 @@
-export default function Maintenace() {
+export default function Maintenance() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="flex flex-col text-background gap-6 bg-orange-200 p-8 rounded-xl border">
         <h1 className="text-5xl font-bold text tracking-tight text-orange-400/80">
-          Maintenace 🚧
+          Maintenance 🚧
         </h1>
         <p className="text-base font-normal">
-          This page is in Maintenace. Please check back later.
+          This page is in Maintenance. Please check back later.
         </p>
       </section>
     </main>
