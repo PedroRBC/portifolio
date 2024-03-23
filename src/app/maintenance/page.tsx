e25f767f48506163a1bd9197f1d7b8f085f34399
Pedro Rodrigues <58788062+PedroRBC@users.noreply.github.com>
2024-03-23 19:27:31 -0300

    chore: Fix color in text's frin maintenance

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,7 +1,7 @@
 export default function Maintenance() {
   return (
     <main className="flex flex-1 flex-col items-center justify-between p-24">
-      <section className="flex flex-col text-background gap-6 bg-orange-200 p-8 rounded-xl border">
+      <section className="flex flex-col gap-6 bg-orange-200 p-8 rounded-xl border">
         <h1 className="text-5xl font-bold text tracking-tight text-orange-400/80">
           Manutenção 🚧
         </h1>
