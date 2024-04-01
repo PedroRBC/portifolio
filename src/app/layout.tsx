c4850d7c3b18c0b9a9ecf5e126ed9d6900aa3a17
PedroRBC <pedrinhorbc@gmail.com>
2024-04-01 13:03:45 +0000

    feat: Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,12 @@ import "./globals.css";
 
 import { docsConfig } from "@/config/docs";
 
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Providers } from "@/providers";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: {
     default: docsConfig.siteName,
@@ -36,8 +34,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen w-screen bg-background antialiased text-foreground flex flex-col",
-          inter.className,
+          "min-h-screen w-screen bg-background font-sans antialiased text-foreground flex flex-col",
+          GeistSans.variable,
         )}
       >
         <Providers>
