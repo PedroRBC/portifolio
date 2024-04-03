5ba177cf3c2c3fff7360d7516f1a2fdd9572c5f8
PedroRBC <pedrinhorbc@gmail.com>
2024-04-03 20:13:24 +0000

    create: About simple page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,35 @@
+import { Icons } from "@/components/icons";
+import { siteConfig } from "@/config/site";
+
+export const metadata = {
+  title: "Sobre Min",
+};
+
 export default function AboutPage() {
-  return <main className="flex flex-1"></main>;
+  return (
+    <main className="container flex flex-1 justify-center">
+      <section className="flex flex-col justify-around">
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-bold">Habilidades</h1>
+          <div className="flex gap-5 my-7 items-center justify-center flex-wrap">
+            {siteConfig.skills.map((skill) => {
+              const Icon = Icons[skill.icon];
+              return (
+                <div
+                  key={skill.name}
+                  className="flex justify-center gap-y-1 flex-col items-center"
+                >
+                  <Icon className="w-10 h-10" />
+                  <span>{skill.name}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex items-center">
+          <h2 className="text-xl font-mono">Em construção... 🔥🔥🔥</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,21 @@
-export const siteConfig = {
+import { Icons } from "@/components/icons";
+
+export type SiteConfig = {
+  name: string;
+  description: string;
+  userIcon: string;
+  socials: {
+    twitter: string;
+    github: string;
+    mail: string;
+  };
+  skills: {
+    name: string;
+    icon: keyof typeof Icons;
+  }[];
+};
+
+export const siteConfig: SiteConfig = {
   name: "Pedro R.",
   description:
     "Portfolio pessoal do Pedro, desenvolvedor e estudante de programação.",
@@ -8,6 +25,22 @@ export const siteConfig = {
     github: "https://github.com/pedrorbc",
     mail: "mailto:contato@pedrorbc.com",
   },
+  skills: [
+    {
+      name: "React",
+      icon: "react",
+    },
+    {
+      name: "Next",
+      icon: "next",
+    },
+    {
+      name: "Typescript",
+      icon: "typeScript",
+    },
+    {
+      name: "Prisma",
+      icon: "prisma",
+    },
+  ],
 };
-
-export type SiteConfig = typeof siteConfig;
