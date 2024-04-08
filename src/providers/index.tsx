3b966ef9f5d3bb2a88d8119391ff6b7eb0830b62
PedroRBC <pedrinhorbc@gmail.com>
2024-04-08 01:07:00 +0000

    feat: Add analytics

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,7 @@
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { ThemeProvider } from "./ThemeProvider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         {children}
         <TailwindIndicator />
         <SpeedInsights />
+        <Analytics />
       </ThemeProvider>
     </>
   );
